59a370ebbe04dac93f22a655a5d3122b66b3cc77
Rafael Varago <rafael.varago@gmail.com>
2023-04-25 15:17:14 +0200

    Add decoder `ipv4` for IPv4 addresses

--- src/elements/mod.rs
+++ src/elements/mod.rs
@@ -2,6 +2,11 @@
 
 pub mod ints;
 
+use super::combinators::DecoderExt;
+
+use std::{io, net::Ipv4Addr};
+use tokio_util::codec::Decoder;
+
 /// Shorthand notation to construct an [`ints::U8`].
 pub fn uint8() -> ints::U8 {
     ints::U8::default()
@@ -25,4 +30,26 @@ pub fn uint32_be() -> ints::U32BE {
 /// Shorthand notation to construct an [`ints::U32LE`].
 pub fn uint32_le() -> ints::U32LE {
     ints::U32LE::default()
+}
+
+/// Codec for an [`Ipv4Addr`] as a sequence of four contiguous [`u8`].
+///
+/// # Examples
+///
+/// ```
+/// # use bytes::BytesMut;
+/// # use tokio_util::codec::Decoder;
+/// # use tokio_util_codec_compose::elements::ipv4;
+/// let mut decoder = ipv4();
+///
+/// let res = decoder.decode(&mut BytesMut::from("\x42\x66\x07\x63")).unwrap();
+///
+/// assert_eq!(res, "66.102.7.99".parse().ok())
+/// ```
+pub fn ipv4() -> impl Decoder<Item = Ipv4Addr, Error = io::Error> {
+    uint8()
+        .then(uint8())
+        .then(uint8())
+        .then(uint8())
+        .map(|(((a, b), c), d)| Ipv4Addr::new(a, b, c, d))
 }
