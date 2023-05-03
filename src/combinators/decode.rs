fcd95b8a9f35cfdf5ef956c429cc673dbf87a779
Rafael Varago <rafael.varago@gmail.com>
2023-05-03 17:03:10 +0200

    Add `DecoderExt::try_map` combinator
    For when we want to interleave decoding with validation.

--- src/combinators/decode.rs
+++ src/combinators/decode.rs
@@ -7,7 +7,7 @@ use tokio_util::codec::Decoder;
 pub trait DecoderExt<A, E>: Decoder<Item = A, Error = E> {
     /// Applies a function `f` of type `A -> B` over the decoded value when that is `Ok(Some(a))`.
     ///
-    /// The function `f` cannot fail.
+    /// The function `f` cannot fail. If you need a fallible mapping, then consider [`DecoderExt::try_map`].
     ///
     /// # Examples
     ///
@@ -29,6 +29,57 @@ pub trait DecoderExt<A, E>: Decoder<Item = A, Error = E> {
         Self: Sized,
     {
         DecoderMap { inner: self, f }
+    }
+
+    /// Applies a fallible function `f` of type `A -> Result<B, EE>` over the decoded value when that is `Ok(Some(a))`.
+    ///
+    /// The function `f` can fail and that's handy when we interleave decoding with validation,
+    /// for instance, when mapping from a larger domain (e.g. `u8`) into a smaller co-domain (e.g. `Version::v1`).
+    ///
+    /// The mapping can return an error type `EE` other than `E` as long as there is an implicit conversion [`From<E>`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use tokio_util::codec::Decoder;
+    /// # use bytes::BytesMut;
+    /// use tokio_util_codec_compose::{combinators::DecoderExt, elements::uint8};
+    ///
+    /// # #[derive(Debug, PartialEq, Eq)]
+    /// enum Version {
+    ///     V1
+    /// }
+    ///
+    /// impl TryFrom<u8> for Version {
+    ///     type Error = std::io::Error;
+    ///
+    ///     fn try_from(value: u8) -> Result<Self, Self::Error> {
+    ///             match value {
+    ///                 1 => Ok(Version::V1),
+    ///                 _ => Err(std::io::Error::from(std::io::ErrorKind::InvalidData))
+    ///             }
+    ///     }
+    /// }
+    ///
+    /// let mut decoder = uint8().try_map(Version::try_from);
+    ///
+    /// let version_ok = decoder.decode(&mut BytesMut::from("\x01")).unwrap();
+    /// assert_eq!(version_ok, Some(Version::V1));
+    ///
+    /// let version_err = decoder.decode(&mut BytesMut::from("\x02")).unwrap_err();
+    /// assert_eq!(version_err.kind(), std::io::ErrorKind::InvalidData);
+    /// ```
+    fn try_map<F, B, EE>(self, f: F) -> DecoderTryMap<Self, F, EE>
+    where
+        F: Fn(A) -> Result<B, EE>,
+        F: 'static,
+        Self: Sized,
+    {
+        DecoderTryMap {
+            inner: self,
+            f,
+            _error: PhantomData,
+        }
     }
 
     /// Chains a decoder of `B` on the *remaining* bytes after applying this decoder, then returns a pair of the individual values `(a, b)`.
@@ -161,6 +212,29 @@ where
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         Ok(self.inner.decode(src)?.map(&self.f))
+    }
+}
+
+#[derive(Debug)]
+pub struct DecoderTryMap<D, F, E> {
+    inner: D,
+    f: F,
+    _error: PhantomData<E>,
+}
+
+impl<D, F, A, B, E, EE> Decoder for DecoderTryMap<D, F, EE>
+where
+    D: Decoder<Item = A, Error = E>,
+    F: Fn(A) -> Result<B, EE>,
+    E: From<io::Error>,
+    EE: From<io::Error> + From<E>,
+{
+    type Item = B;
+
+    type Error = EE;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        self.inner.decode(src)?.map(&self.f).transpose()
     }
 }
 
@@ -311,6 +385,27 @@ mod tests {
 
     fn bytes() -> impl Strategy<Value = Vec<u8>> {
         proptest::collection::vec(any::<u8>(), 0..255)
+    }
+
+    #[test]
+    fn decode_try_map_succeed() {
+        let mut decoder = uint8().try_map(|_| Ok::<_, io::Error>(0x02u8));
+
+        let mut src = BytesMut::from("\x01");
+        let ok = decoder.decode(&mut src);
+
+        assert!(matches!(ok, Ok(Some(0x02))));
+    }
+
+    #[test]
+    fn decode_try_map_fail() {
+        let mut decoder = uint8().try_map(|_| Err::<u8, _>(io::Error::from(io::ErrorKind::Other)));
+
+        let mut src = BytesMut::from("\x01");
+        let err = decoder.decode(&mut src);
+        let err_kind = err.map_err(|e| e.kind());
+
+        assert!(matches!(err_kind, Err(io::ErrorKind::Other)));
     }
 
     #[test]
